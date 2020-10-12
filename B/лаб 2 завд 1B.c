ef82d8b02291719cc37cd0c5a687af30de9660b2
IlyaLavern <65504519+IlyaLavern@users.noreply.github.com>
2020-10-12 20:12:14 +0300

    Add files via upload

---
+++ B/лаб 2 завд 1B.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+int main()
+{
+  setlocale(LC_CTYPE, "ukr");
+  int o,s,t;
+
+  printf("Введiть число s= \n");
+  scanf("%d", &s);
+
+  printf("Введiть число t= \n");
+  scanf("%d", &t);
+
+
+  if (s==-7)
+  {
+     o=(s/t)+2*s*t;
+      printf("Виконується перша умова\no=%d ", &o);
+  }
+  else if (s==7)
+{
+        o=sqrt(s/t+2*s*t);
+         printf("Виконується друга умова\no=%d ", &o);
+  }
+   else if (s==14)
+  {
+      o=s*s+2*t;
+      printf("Виконується третя умова\no=%d ", &o);
+  }
+  if (s!=-7 && s!=7 && s!=14)
+      printf("Умова не виконується!");
+
+  return 0;
+}
+
